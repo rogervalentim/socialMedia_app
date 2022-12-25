0d244f4769a5fd5ad68608d3b5e0af0177cbb500
rogervalentim <valentim.roger33@gmail.com>
2022-12-25 19:54:02 -0300

    modified package.json

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ import postsRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
 import { createPost } from './controllers/posts.js';
 import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from './data/index.js';
 
 // configurations   
 const __filename = fileURLToPath(import.meta.url);
@@ -55,5 +58,9 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
-    app.listen(PORT, () => console.log(`Server Port:${PORT}`))
-}).catch((error) => console.log(`${error} did not connect`))
+    app.listen(PORT, () => console.log(`Server Port:${PORT}`));
+
+// Add data one time
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+}).catch((error) => console.log(`${error} did not connect`));
